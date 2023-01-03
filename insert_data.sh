8bb357d1dc219a563e227f77eee133e4ea7cddea
skembr01 <samembry7@gmail.com>
2023-01-03 16:04:59 -0500

    feat: get variables from python script to sh

--- insert_data.sh
+++ insert_data.sh
@@ -2,3 +2,26 @@
 
 PSQL="psql --username=sam --dbname=longview -t --no-align -c"
 
+OUTPUT=( $(python3 payroll_2023) )
+# echo $OUTPUT
+# echo ${OUTPUT[1]}
+# # NAME=${OUTPUT[0]}
+# # echo $NAME
+LEN=${#OUTPUT[@]}
+
+NAME=${OUTPUT[0]}
+# $OUTPUT | read NAME GROSS SOCIAL MED STATE FED PAY 
+GROSS=${OUTPUT[1]}
+SOCIAL=${OUTPUT[2]}
+MED=${OUTPUT[3]}
+STATE=${OUTPUT[4]}
+FED=${OUTPUT[5]}
+PAY=${OUTPUT[6]}
+
+if [[ $NAME == 'mike' ]]
+then 
+    CHILD=58.20
+    echo $NAME $GROSS $SOCIAL $MED $STATE $FED $CHILD $PAY
+else 
+    echo $NAME $GROSS $SOCIAL $MED $STATE $FED $PAY
+fi
